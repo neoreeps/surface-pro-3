687187966eb25b3c57fb2627a4030821cde88694
Kenny Speer <kenny.speer@gmail.com>
2015-10-31 12:20:26 -0700

    fix path to patches

--- build_linux.sh
+++ build_linux.sh
@@ -6,11 +6,11 @@ mv linux linux.old
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 
 cd linux
-patch -p1 --ignore-whitespace -i 0001-base-packaging.patch
-patch -p1 --ignore-whitespace -i 0002-debian-changelog.patch
-patch -p1 --ignore-whitespace -i 0003-configs-based-on-Ubuntu-4.3.0-1.5.patch
-patch -p1 --ignore-whitespace -i 0004-surface-cam.patch
-patch -p1 --ignore-whitespace -i 0005-surface-touchpad.patch
+patch -p1 --ignore-whitespace -i ../0001-base-packaging.patch
+patch -p1 --ignore-whitespace -i ../0002-debian-changelog.patch
+patch -p1 --ignore-whitespace -i ../0003-configs-based-on-Ubuntu-4.3.0-1.5.patch
+patch -p1 --ignore-whitespace -i ../0004-surface-cam.patch
+patch -p1 --ignore-whitespace -i ../0005-surface-touchpad.patch
 
 yes '' | make oldconfig
 make clean
